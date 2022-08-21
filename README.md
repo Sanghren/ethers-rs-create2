f57449a6dfd2dd757e7c0b5d01dac40f4a74e719
tbrunain <dev@bouh.io>
2022-08-21 15:55:05 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ethers-rs-create2
-Tests around create2 and ethers
+
+Tests around create2 and ethers.
+
+## Issue with Trader Joe
+
+Right now I am testing how to get the pair address for Trader Joe.
+
+According
+to [their sdk](https://github.com/traderjoe-xyz/joe-sdk/blob/9ea22ece0253e5769cd32c89f9ce9ab8540c4264/src/entities/pair.ts#L39)
+, this are the parameters used in the `getCreate2` call:
+
+```typescript
+getCreate2Address(
+    FACTORY_ADDRESS[chainId],
+    keccak256(['bytes'], [pack(['address', 'address'], [tokens[0].address, tokens[1].address])]),
+    INIT_CODE_HASH[chainId]
+)
+```
+
+where:
+
+- [FACTORY_ADDRESS[chainId]](https://github.com/traderjoe-xyz/joe-sdk/blob/9ea22ece0253e5769cd32c89f9ce9ab8540c4264/src/constants.ts#L27)
+  => `0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10`
+- [INIT_CODE_HASH[chainId]](https://github.com/traderjoe-xyz/joe-sdk/blob/9ea22ece0253e5769cd32c89f9ce9ab8540c4264/src/constants.ts#L157)
+  => `0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91`
+
+I quickly tested this function in typescript to see what would be the outcome, with the following token addresses:
+
+- `0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7` (wavax)
+- `0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E` (usdc)
+
+Is returning the correct pair address `0xf4003F4efBE8691B60249E6afbD307aBE7758adb` .
+
+Now I adapted the [example above](https://github.com/gakonst/ethers-rs/blob/49b4ac7acbbd71b4d10f7780c8d2efe9af27799b/ethers-core/src/utils/mod.rs#L251) `get_create2_address_from_hash` function in ethers.
+
+It returns correct value for the example, generating the expected address on ethereum for the weth/usdc pool address.
+
+But when I try to do the same for a pool on trader joe, it won't give me the expected result. I tried several things but none worked.
+
+Am quite sure I did a woopsie somewhere, but I can't find it.
--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,33 @@ use ethers_core::abi::Token;
 use ethers_core::utils::{get_create2_address, get_create2_address_from_hash, hex, keccak256};
 
 
-
 fn main() {
     /// Test for Uniswap pair USDC/wETH
-    get_pool_pair_address_with_create2(
+    get_pool_pair_address_with_create2_with_fee(
         "e34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54",
         "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
         "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-        "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
+        "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640",
     );
 
     /// Test for TraderJoe pair USDC/wAVAX
     get_pool_pair_address_with_create2(
         "0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91",
         "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10",
-        "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
-        "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e",
-        "0xf4003f4efbe8691b60249e6afbd307abe7758adb"
+        "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+        "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+        "0xf4003f4efbe8691b60249e6afbd307abe7758adb",
     );
 }
 
-fn get_pool_pair_address_with_create2(
+fn get_pool_pair_address_with_create2_with_fee(
     init_code: &str,
     factory_address: &str,
     token0_address: &str,
     token1_address: &str,
     expected_result: &str) {
-    let UNISWAP_V3_POOL_INIT_CODE_HASH = Bytes::from(
+    let INIT_CODE_HASH = Bytes::from(
         hex::decode(init_code).unwrap(),
     );
     let factory: Address = factory_address
@@ -53,25 +52,22 @@ fn get_pool_pair_address_with_create2(
     ]);
 
     let salt = keccak256(&input);
-    let pool_address =
-        get_create2_address_from_hash(factory, salt.to_vec(), UNISWAP_V3_POOL_INIT_CODE_HASH);
+    println!("-------\nFactory {:?} \nInput {:?}\nSalt {:?}\ninit_code {:?}", factory, input, salt, hex::decode(init_code).unwrap());
 
-    assert_eq!(
-        pool_address,
-        expected_result
-            .parse()
-            .unwrap()
-    );
-    println!("POOL ADDRESS {:?}", pool_address);
+    let pool_address =
+        get_create2_address_from_hash(factory, salt.to_vec(), INIT_CODE_HASH);
+
+
+    println!("POOL ADDRESS {:?} // {:?}", pool_address, expected_result);
 }
 
-fn get_pool_pair_address_with_create2_hash(
+fn get_pool_pair_address_with_create2(
     init_code: &str,
     factory_address: &str,
     token0_address: &str,
     token1_address: &str,
     expected_result: &str) {
-    let UNISWAP_V3_POOL_INIT_CODE_HASH = Bytes::from(
+    let INIT_CODE_HASH = Bytes::from(
         hex::decode(init_code).unwrap(),
     );
     let factory: Address = factory_address
@@ -86,18 +82,14 @@ fn get_pool_pair_address_with_create2_hash(
 
     let input = abi::encode(&vec![
         Token::Address(token0),
-        Token::Address(token1)
+        Token::Address(token1),
     ]);
 
     let salt = keccak256(&input);
-    let pool_address =
-        get_create2_address(factory, salt.to_vec(), Bytes::from(init_code));
+    println!("-------\nFactory {:?} \nInput {:?}\nSalt {:?}\ninit_code {:?}", factory, input, salt, INIT_CODE_HASH);
 
-    assert_eq!(
-        pool_address,
-        expected_result
-            .parse()
-            .unwrap()
-    );
-    println!("POOL ADDRESS {:?}", pool_address);
+    let pool_address =
+        get_create2_address_from_hash(factory, salt.to_vec(), INIT_CODE_HASH);
+
+    println!("POOL ADDRESS {:?} // {:?}", pool_address, expected_result);
 }
